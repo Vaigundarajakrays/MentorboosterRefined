dfb5ec3924652fe38b19efb02dd0dfd0b653cc10
Vaigunda <vaigundaraja.krays@gmail.com>
2025-07-31 18:08:26 +0530

    Updated secret name

--- src/main/java/com/mentorboosters/app/zoom/SecretCheck.java
+++ src/main/java/com/mentorboosters/app/zoom/SecretCheck.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class SecretCheck {
 
+    // checking
     @Value("${stripe.webhook.secret}")
     private String stripeApiKey;
 
