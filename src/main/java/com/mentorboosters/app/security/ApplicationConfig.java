5e023a2ffb6d0ae26e82c2220b9a3041e7205fe3
Vaigunda <vaigundaraja.krays@gmail.com>
2025-07-31 17:38:04 +0530

    Added awss secret manger

--- src/main/java/com/mentorboosters/app/security/ApplicationConfig.java
+++ src/main/java/com/mentorboosters/app/security/ApplicationConfig.java
@@ -5,6 +5,7 @@ import com.mentorboosters.app.model.Users;
 import com.mentorboosters.app.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -21,6 +22,14 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class ApplicationConfig {
 
     private final UsersRepository usersRepository;
+
+    // To avoid this JwtAuthenticationFilter to be added as a filter in servlet container
+    @Bean
+    public FilterRegistrationBean<JwtAuthenticationFilter> jwtAuthFilterRegistration(JwtAuthenticationFilter filter) {
+        FilterRegistrationBean<JwtAuthenticationFilter> registration = new FilterRegistrationBean<>(filter);
+        registration.setEnabled(false); // Prevents container from auto-registering it
+        return registration;
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
---
+++ src/main/java/com/mentorboosters/app/zoom/SecretCheck.java
@@ -0,0 +1,19 @@
+package com.mentorboosters.app.zoom;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+
+
+@Component
+public class SecretCheck {
+
+    @Value("${stripe.webhook.secret}")
+    private String stripeApiKey;
+
+    @PostConstruct
+    public void showSecret() {
+        System.out.println("🔥 Stripe API Key from AWS Secrets Manager: " + stripeApiKey);
+    }
+}
